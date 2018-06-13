e60d9280d6f48fbaf05f218b3ff0d4adede960f9
Mariam Chaudry <mariamschaudry1@gmail.com>
2018-06-13 19:30:02 -0400

    added all of portfolio

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
+// import Wow from "./JS/wow.js";
+
+
+// -------- //
+// Parallax 
+// -------- //
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
     $('.parallax').parallax();
-  });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+// Wow Animations //
+
+wow = new Wow({
+  mobile: false // <--- Not applicable to mobile scroll settings //
+});
+wow.init();
